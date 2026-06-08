d8581b2d6faa6a027904abbd47c1611ec2e84041
jendrick19 <jendrickmontiel18@gmail.com>
2026-06-08 07:40:12 -0400

    feat

--- include/nodo.h
+++ include/nodo.h
@@ -9,7 +9,8 @@ class nodo {
 public:
     nodo() { der = NULL; }
     void AsigInfo(Tipo v) { info = v; }
-    Tipo ObtInfo() { return info; }
+    Tipo& ObtInfo() { return info; }
+    const Tipo& ObtInfo() const { return info; }
     void AsigDer(nodo<Tipo> *p) { der = p; }
     nodo<Tipo>* ObtDer() { return der; }
     void AsigSig(nodo<Tipo> *p) { der = p; }
